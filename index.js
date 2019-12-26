a3191e3e2f5552a4915d513069598e49a558e1b8
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-12-26 20:22:58 +0530

    Added logger middleware

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
+const reduxLogegr = require('redux-logger');
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const createLogger = reduxLogegr.createLogger;
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -54,9 +58,11 @@ const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream: iceCreamReducer
 });
-const store = createStore(rootReducer);
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial State', store.getState());
-const unsubscribe = store.subscribe(() => console.log('Update State', store.getState()));
+const unsubscribe = store.subscribe(() => { });
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
