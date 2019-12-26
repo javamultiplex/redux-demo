8e11f9810eeda396d2ec69b3faf6efd847c5048b
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-12-26 19:00:17 +0530

    Added one more property in state

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake() {
     return {
@@ -10,8 +11,15 @@ function buyCake() {
     }
 }
 
+function buyIcecream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
 const initialState = {
-    numberOfCakes: 10
+    numberOfCakes: 10,
+    numberOfIcecreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -20,6 +28,11 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 numberOfCakes: state.numberOfCakes - 1
+            }
+        case 'BUY_ICECREAM':
+            return {
+                ...state,
+                numberOfIcecreams: state.numberOfIcecreams - 1
             }
         default:
             return state;
@@ -31,5 +44,9 @@ console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(() => console.log('Update State', store.getState()));
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIcecream());
+store.dispatch(buyIcecream());
+store.dispatch(buyIcecream());
 store.dispatch(buyCake());
 unsubscribe();
